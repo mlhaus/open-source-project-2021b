5d37ce222e8af20bbb10a07133f4e84cc0716d1d
Logan B <k0832049@student.kirkwood.edu>
2021-10-20 13:23:10 -0500

    Adding Main class

---
+++ src/com/hauschildt/Main.java
@@ -0,0 +1,20 @@
+package com.hauschildt;
+
+public class Main {
+
+    public static void main(String [] args){
+        Order order = new Order();
+
+        order.addItem(PizzaItem.CHEESE.getItem());
+        order.addItem(PizzaItem.PEPPERONI.getItem());
+        order.addItem(PizzaItem.THE_WORKS.getItem());
+        order.addItem(PizzaItem.CHICKEN_ALFREDO.getItem());
+
+        System.out.println(order.getNumberOfItems());
+        System.out.println(order.getItem(0).getName());
+        System.out.println(order.getItem(1).getName());
+        System.out.println(order.getItem(2).getName());
+        System.out.println(order.getItem(3).getName());
+
+    }
+}
